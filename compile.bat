89182aa60ebd41cc83aefcbd54c1ca4fc4f40e36
Witold Hołdys <152323163+WitHol@users.noreply.github.com>
2024-08-27 21:57:33 +0200

    Removed file.txt and changed the executable name

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-gcc src/main.cpp src/editor_window/editor_window.cpp src/key_detection/key_detection-windows.cpp src/extras/extras.cpp -o main.exe -lncursesw -DNCURSES_STATIC -lstdc++
+gcc src/main.cpp src/editor_window/editor_window.cpp src/key_detection/key_detection-windows.cpp src/extras/extras.cpp -o wited.exe -lncursesw -DNCURSES_STATIC -lstdc++
